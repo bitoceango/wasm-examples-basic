66625f5d9b361e9d73031e3e0ab7166573fabeb3
xingyuntian@lixiang.com <xingyuntian@lixiang.com>
2022-04-18 11:31:52 +0800

    wasm_bindgen pkg

--- rust-wasm/target/rls/debug/deps/hello_world-846a6d9831776d58.d
+++
@@ -1,5 +0,0 @@
-/Users/xingyuntian/develop/bitocean/wasm-examples-basic/rust-wasm/target/rls/debug/deps/hello_world-846a6d9831776d58.rmeta: src/lib.rs
-
-/Users/xingyuntian/develop/bitocean/wasm-examples-basic/rust-wasm/target/rls/debug/deps/hello_world-846a6d9831776d58.d: src/lib.rs
-
-src/lib.rs:
